73a886244654ea28483cd2efa1779e301cd059ee
Snehal <snehalgadge205@gmail.com>
2023-04-27 19:36:01 +0530

    intersection

--- 03_Array/09_Union.cpp
+++ 03_Array/09_Union.cpp
@@ -33,6 +33,28 @@ vector < int > FindUnion(int arr1[], int arr2[], int n, int m) {
   return Union;
 }
 
+vector<int> FindIntersection(vector<int>A, vector<int>B){
+    
+  vector <int> ans;
+  
+  // to maintain visited
+  vector <int> visited(B.size(), 0);  
+  int i = 0, j = 0;
+  
+  while(i<A.size() && j<B.size()){
+    if(A[i]<B[j]) i++;
+    else if(B[j]<A[i]) j++;
+    else{
+      ans.push_back(arr[i]);
+      i++; j++;
+    }
+  }
+
+  return ans;
+  
+    
+}
+
 int main()
 
 {
@@ -42,6 +64,14 @@ int main()
   vector < int > Union = FindUnion(arr1, arr2, n, m);
   cout << "Union of arr1 and arr2 is  " << endl;
   for (auto & val: Union)
+    cout << val << " ";
+
+  vector < int > A {1,2,3,3,4,5,6,7};
+  vector < int > B {3,3,4,4,5,8};
+
+  vector < int > Intersection = FindIntersection(arr1, arr2);
+  cout <<"Intersection of arr1 and arr2 is  " << endl;
+  for (auto & val: Intersection)
     cout << val << " ";
   return 0;
 }
