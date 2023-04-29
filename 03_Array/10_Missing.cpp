44bc960d372f535800dff75f7aff1f31303095a1
Snehal <snehalgadge205@gmail.com>
2023-04-29 22:25:09 +0530

    changes made

--- 03_Array/10_Missing.cpp
+++ 03_Array/10_Missing.cpp
@@ -2,19 +2,31 @@
 using namespace std;
 
 int findMissing(int arr[] , int n){
+    // Loop :- O(n^2)[Hypothetical]
+    // for(int i=1; i<=n; i++){
+    //     int flag = 0 ;
+    //     for(int j=0; j<n-1; j++){
+    //         if(arr[j] == i){
+    //             flag = 1;
+    //             break;
+    //         }
+    //     }
 
-    for(int i=0; i<n-1; i++){
+    //     if(flag == 0) return i;
+    // }
+    // return -1;
 
-        if(arr[i] != i+1) return i+1;
-        
-    }
 
+    // 
+
+    
 }
 
 int main(){
 
     int arr[] = {1,2,4,5};
-    int ans = findMissing(arr,5);
+    int ans = findMissing(arr,4);
     cout<<ans;
 
 }
+
