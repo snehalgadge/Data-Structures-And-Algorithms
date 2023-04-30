875c527d35ea10db2c1887b3b2ec388bc4a6ce7f
Snehal <snehalgadge205@gmail.com>
2023-04-30 20:35:45 +0530

    single number all solutions added

--- 03_Array/12_SingleNumber.cpp
+++ 03_Array/12_SingleNumber.cpp
@@ -4,18 +4,48 @@ using namespace std;
 int getSingleElement(vector<int> arr){
     int n = arr.size();
 
-    for(int i=0; i<n; i++){
-        int count = 0;
-        for(int j=0; j<n; j++){
-            if(arr[i] == arr[j]){
-                count++;
-            }
-        }
+    //  1)BRUTE FORCE
+    // for(int i=0; i<n; i++){
+    //     int count = 0;
+    //     for(int j=0; j<n; j++){
+    //         if(arr[i] == arr[j]){
+    //             count++;
+    //         }
+    //     }
 
-        if(count == 1) return arr[i];
-    }
-    return -1;
+    //     if(count == 1) return arr[i];
+    // }
+    // return -1;
+
+// 2)BETTER
+    // int maxi = arr[0];
+    // for(int i=0; i<n; i++){
+    //    maxi = max(maxi , arr[i]);
+    // }
+
+    // vector<int> hash(maxi+1, 0);
+
+    // for(int i=0; i<n; i++){
+    //     hash[arr[i]]++;
+    // }
+
+    // for(int i=0; i<n; i++){
+    //     if(hash[arr[i]] == 1)
+    //     return arr[i];
+    // }
+
+    // return -1;
+
+   
+// 3)OPTIMAL
+//        int xxor = 0;
+//        for(int i=0; i<n; i++){
+//           xxor = xxor^arr[i];
+//         }
+//         return xxor;
+
 }
+
 int main()
 {
     vector<int> arr = {4, 1, 2, 1, 2};
