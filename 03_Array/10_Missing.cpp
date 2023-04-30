263db409a0afb481b1481960420abf71ccd9a083
Snehal <snehalgadge205@gmail.com>
2023-04-30 19:25:27 +0530

    maximum consecutive ones

--- 03_Array/10_Missing.cpp
+++ 03_Array/10_Missing.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int findMissing(int arr[] , int n){
+
     // Loop :- O(n^2)[Hypothetical]
     // for(int i=1; i<=n; i++){
     //     int flag = 0 ;
@@ -14,18 +15,32 @@ int findMissing(int arr[] , int n){
 
     //     if(flag == 0) return i;
     // }
-    // return -1;
+  
 
 
-    // 
+    // 2.Hashing :- O(n)
+    // int hash[n+1] = {0};
+
+    // for(int i=0; i<n-1; i++){
+    //     hash[arr[i]]++;
+    // }
+
+    // for(int i=1; i<=n; i++){
+    //     if(hash[i] == 0) {
+    //         return i;
+    //     }
+    // }
 
     
+    // 3.Using XOR
+    
+    return -1;
 }
 
 int main(){
 
     int arr[] = {1,2,4,5};
-    int ans = findMissing(arr,4);
+    int ans = findMissing(arr,5);
     cout<<ans;
 
 }
